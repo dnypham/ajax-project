1b30cf7727b661e0a1c2c44121dfe6a2116a5722
Daniel Pham <dnypham@gmail.com>
2021-09-22 18:51:47 +0000

    Add view switching between Homepage View and Local Breweries View.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ var $linkedin = document.querySelector('#linkedin');
 $linkedin.addEventListener('click', function () {
   window.open('https://www.linkedin.com/in/daniel-pham-10/');
 });
+
+var $homepage = document.querySelector('h1');
+var $homepageView = document.querySelector('main[data-view="homepage"]');
+var $localButton = document.querySelector('#local-button');
+var $localView = document.querySelector('main[data-view="local"]');
+
+$homepage.addEventListener('click', function () {
+  $homepageView.classList.remove('hidden');
+  $localView.classList.add('hidden');
+});
+
+$localButton.addEventListener('click', function (event) {
+  $localView.classList.remove('hidden');
+  $homepageView.classList.add('hidden');
+});
