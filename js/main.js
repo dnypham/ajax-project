5c34bf8e95e2b0d14abc7a96ec97299ab492480b
Daniel Pham <dnypham@gmail.com>
2021-09-22 19:46:28 +0000

    Create render function template.

--- js/main.js
+++ js/main.js
@@ -23,4 +23,90 @@ $homepage.addEventListener('click', function () {
 $localButton.addEventListener('click', function (event) {
   $localView.classList.remove('hidden');
   $homepageView.classList.add('hidden');
+
+  renderBreweries();
 });
+
+var $parentDiv = document.querySelector('#parentDiv');
+
+function renderBreweries() {
+  var $breweryCardContainer = document.createElement('div');
+  $breweryCardContainer.setAttribute('class', 'brewery-card-flex');
+
+  var $breweryCard = document.createElement('div');
+  $breweryCard.setAttribute('class', 'brewery-card');
+  $breweryCardContainer.appendChild($breweryCard);
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $breweryCard.appendChild($row);
+
+  var $col1 = document.createElement('div');
+  $col1.setAttribute('class', 'column-half');
+  $row.appendChild($col1);
+
+  var $infoFlex = document.createElement('div');
+  $infoFlex.setAttribute('class', 'info-flex');
+  $col1.appendChild($infoFlex);
+
+  var $breweryInfo = document.createElement('div');
+  $breweryInfo.setAttribute('class', 'brewery-info');
+  $infoFlex.appendChild($breweryInfo);
+
+  var $h3 = document.createElement('h3');
+  $h3.textContent = 'Common Space Brewery';
+  $breweryInfo.appendChild($h3);
+
+  var $ul = document.createElement('ul');
+  $breweryInfo.appendChild($ul);
+
+  var $li1 = document.createElement('li');
+  $li1.textContent = 'Hawthorne';
+  $ul.appendChild($li1);
+
+  var $li2 = document.createElement('li');
+  $li2.textContent = '3411 El Segundo Blvd';
+  $ul.appendChild($li2);
+
+  var $li3 = document.createElement('li');
+  $li3.textContent = '(' + '310' + ')' + '-' + '662' + '-' + '825';
+  $ul.appendChild($li3);
+
+  var $li4 = document.createElement('li');
+  $li4.textContent = 'Website';
+  $ul.appendChild($li4);
+
+  var $listIconsFlex = document.createElement('div');
+  $listIconsFlex.setAttribute('class', 'list-icons-flex');
+  $breweryInfo.appendChild($listIconsFlex);
+
+  var $i1 = document.createElement('i');
+  $i1.setAttribute('class', 'far fa-bookmark fa-2x');
+  $listIconsFlex.appendChild($i1);
+
+  var $i2 = document.createElement('i');
+  $i2.setAttribute('class', 'far fa-heart fa-2x');
+  $listIconsFlex.appendChild($i2);
+
+  var $col2 = document.createElement('div');
+  $col2.setAttribute('class', 'column-half');
+  $row.appendChild($col2);
+
+  var $mapFlex = document.createElement('div');
+  $mapFlex.setAttribute('class', 'map-flex');
+  $col2.appendChild($mapFlex);
+
+  var $breweryMap = document.createElement('div');
+  $breweryMap.setAttribute('class', 'brewery-map');
+  $mapFlex.appendChild($breweryMap);
+
+  var $map = document.createElement('iframe');
+  $map.setAttribute('src', 'https://maps.google.com/maps?q=33.91683, -118.33350&z=15&output=embed');
+  $map.setAttribute('width', '228');
+  $map.setAttribute('height', '168');
+  $map.setAttribute('frameborder', '0');
+  $map.setAttribute('style', 'border: 0;');
+  $breweryMap.appendChild($map);
+
+  $parentDiv.appendChild($breweryCardContainer);
+}
