5d8f2f1d1c99d523c6ba9e800d4ee4ff17778a3b
Daniel Pham <dnypham@gmail.com>
2021-09-22 16:39:08 +0000

    Add event listener to LinkedIn icon to go to LinkedIn profile.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ var $github = document.querySelector('#github');
 $github.addEventListener('click', function () {
   window.open('https://github.com/dnypham');
 });
+
+var $linkedin = document.querySelector('#linkedin');
+
+$linkedin.addEventListener('click', function () {
+  window.open('https://www.linkedin.com/in/daniel-pham-10/');
+});
